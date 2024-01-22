bd742442da9473b12e02621d3d073b3a76a35664
Gledesonsc <elias100corithians@hotmail.com>
2024-01-22 16:25:03 -0300

    feat: add script index

---
+++ script.js
@@ -0,0 +1,18 @@
+function shareUrl() {
+  const shareData = {
+      title: 'Título do Compartilhamento',
+      text: 'Texto do Compartilhamento',
+      url: 'https://adalinks.vercel.app',
+  };
+
+  if (navigator.share) {
+      // Se o navegador suportar a Web Share API
+      navigator.share(shareData)
+          .then(() => console.log('Conteúdo compartilhado com sucesso'))
+          .catch((error) => console.error('Erro ao compartilhar:', error));
+  } else {
+      // Link de fallback para o WhatsApp
+      const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(shareData.text + ' ' + shareData.url)}`;
+      window.open(whatsappUrl, '_blank');
+  }
+}
